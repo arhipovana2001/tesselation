3c38594880143fe86ccb68aace71b78777122b84
arhipovana2001 <55373084+arhipovana2001@users.noreply.github.com>
2020-03-20 12:57:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,27 @@ Developers: Arkhipova A.(%)
             Revtova L.(%)
 '''
 import turtle
+from math import cos, pi, sin
 
 
 def get_color_choice():
-    COLORS = ['Красный', 'Желтый', 'Зеленый', 'Синий', 'Оранжевый', 'Розовый']
-    print('''Допустимые цвета заливки:
-1.Красный\n2.Желтый\n3.Зеленый\n4.Синий\n5.Оранжевый\n6.Розовый\n\nВыберите цвет:''')
+    COLORS = ['red', 'yellow', 'green', 'blue', 'orange', 'pink']
+    print('''Acceptable fill colors:
+1.red\n2.yellow\n3.green\n4.blue\n5.orange\n6.pink\n\nВыберите цвет:''')
     choice = input()
     while choice not in COLORS:
-        print('Неверное значение. Введите снова:')
+        print('Incorrect value. Enter again:')
         choice = input()
     return choice
 
 
 def get_num_hexagons():
     NUMBERS = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-    print('''Допустимое количество шестиугольников от 4 до 20.
-Выберите количество шестиугольников''')
+    print('''The allowed number of hexagons is from 4 to 20.
+Select the number of hexagons:''')
     choice = input()
     while int(choice) not in NUMBERS:
-        print('Неверное значение. Введите снова:')
+        print('Incorrect value. Enter again:')
         choice = input()
     return int(choice)
 
